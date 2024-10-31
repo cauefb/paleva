f9f9709c08cdaa6cb9276ae3108eae16db262c95
Caue Ferreira <ferreira.caue03@gmail.com>
2024-10-31 15:38:23 -0300

    adiciona edição de prato

--- spec/system/dishes/user_edit_dish_spec.rb
+++ spec/system/dishes/user_edit_dish_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
-describe "Usuário vê os pratos cadastrados" do
+describe "Usuário edita um prato" do
   it "com sucesso" do
     #arrange
     user = User.create!(name:"João", last_name:"Campos", cpf: CPF.generate, email: "joao@gmail.com", password: "password123456")
     establishment = Establishment.create!(email:'teste@gmail.com', brand_name: 'teste', corporate_name: 'teste LTDA', 
       cnpj: '56924048000140',phone: '71992594946', address: 'Rua das Alamedas avenidas', user: user)
-     Dish.create!(name: 'Lasanha', description: 'Bolonhesa', calories: '400', establishment: establishment)
-     Dish.create!(name: 'Macarrão', description: 'Bolonhesa', calories: '600', establishment: establishment)
-     Dish.create!(name: 'Risoto', description: 'com muito camarrão', calories: '700', establishment: establishment)
+      Dish.create!(name: 'Lasanha', description: 'Bolonhesa', calories: '400', establishment: establishment)
 
     #act
     visit root_path
@@ -17,9 +15,17 @@ describe "Usuário vê os pratos cadastrados" do
       click_on 'Entrar'
     end
     click_on 'Meus Pratos'
+    click_on 'Ver Detalhes'
+    click_on 'Editar prato'
+    fill_in "Nome",	with: "Lasanha bolonhesa"
+    fill_in "Descrição",	with: "lasanha bolonhessa com muito queijo"  
+    click_on 'Salvar'
+
     #assert
-    expect(page).to have_content 'Lasanha' 
-    expect(page).to have_content 'Bolonhesa'  
-    expect(page).to have_content '400'  
+    expect(page).to have_content 'Prato atualizado com sucesso' 
+    expect(page).to have_content 'Lasanha bolonhesa'  
+    expect(page).to have_content 'lasanha bolonhessa com muito queijo' 
   end
+  
+  
 end
