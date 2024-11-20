e2a64be96ce4298a95696ebd71a62be5f57c0e1a
Caue Ferreira <ferreira.caue03@gmail.com>
2024-11-20 19:16:21 -0300

    adiciona feature ver detalhes do pedido

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::OrdersController < ActionController::API
-  before_action :find_establishment, only: [:index]
+  before_action :find_establishment
+  before_action :find_order, only: [:show]
 
   def index
-      # Verifica se o parâmetro de status é válido
       valid_statuses = Order.statuses.keys
       if params[:status].present? && valid_statuses.include?(params[:status])
         @orders = @establishment.orders.where(status: params[:status])
@@ -13,11 +13,34 @@ class Api::V1::OrdersController < ActionController::API
       render json: @orders, status: :ok
   end
 
+  def show
+    render json: {
+      costumer_name: @order.costumer_name,
+      code: @order.code,
+      items: @order.order_items.map do |item|
+        {
+          name: item.item_name,
+          quantity: item.quantity,
+          unit_price: item.unit_price,
+          observation: item.observation
+        }
+      end,
+      created_at: @order.created_at,
+      status: @order.status
+    }, status: :ok
+  end
+
   private
 
   def find_establishment
     @establishment = Establishment.find_by!(code: params[:establishment_code])
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Estabelecimento não encontrado' }, status: :not_found
+  end
+
+  def find_order
+    @order = @establishment.orders.find_by!(code: params[:order_code])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Pedido não encontrado' }, status: :not_found
   end
 end
